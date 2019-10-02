1e6074e7e2e6b5ea1aa055dac3467412e4c4239f
Anup Dubey <anupdubey@paytm.com>
2019-10-02 15:46:26 +0530

    amilist

--- amilist.py
+++ amilist.py
@@ -2,7 +2,7 @@
 import boto3
 ec2_re=boto3.resource(service_name="ec2",region_name="ap-south-1")
 ec2_cli=boto3.client("sts")
-account_id =ec2_cli.get_caller_identity()["Account"]
+account_id=ec2_cli.get_caller_identity()["Account"]
 print("List of ami available")
 count=1
 for i in ec2_re.images.filter(Owners=[account_id]):
